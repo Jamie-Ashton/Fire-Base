ee7bbfd2c8f711ea19829449b6c1f88f19c6bc78
jamie-noel <jamie-ashton@live.nmit.ac.nz>
2023-10-19 10:55:14 +1300

    boilerplate1

---
+++ firebase-sdk.js
@@ -0,0 +1,20 @@
+// import the fire base finctions 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+// too do add SDKs for fire base products
+// 
+
+
+// note- your web app firebase config
+const firebaseConfig = {
+    apiKey: "AIzaSyBXg04t3matWwV000rri2EDYXWRNQBwPxc",
+    authDomain: "fir-example-fc0ea.firebaseapp.com", 
+    projectId: "fir-example-fc0ea",
+    storageBucket: "fir-example-fc0ea.appspot.com",
+    messagingSenderId: "970257931610",
+    appId: "1:970257931610:web:8adfa6d4a46c097c5f2763"
+  };
+
+//   initialize firebase
+const app = initializeApp(firebaseConfig)
+
+
